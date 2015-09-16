9fc1470a2f25ad293661c1d802a218dd98ba336d
yehchienwei <yehchienwei@hotmail.com>
2015-09-16 11:24:01 -0700

    [Add] Camera script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform Player;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (Player.position.x + 10, Player.position.y, -10);
+	}
+}
