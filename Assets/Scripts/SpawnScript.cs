670d24cfbadb891f23b6d9f948049b085928cf55
yehchienwei <yehchienwei@hotmail.com>
2015-09-17 11:55:20 -0700

    Add[Generate ground]
    隨機產生地圖

---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject[] obj;
+	public Transform Player;
+	public float spawnMin = 1f;
+	public float spawnMax = 2f;
+	void Start () {
+		Spawn ();
+	
+	}
+	
+	// Update is called once per frame
+
+	void Spawn()
+	{
+		Instantiate (obj [Random.Range (0, obj.GetLength(0))], new Vector3(Player.position.x+20,3,0), Quaternion.identity);
+		Invoke ("Spawn",Random.Range(spawnMin,spawnMax));
+	}
+}
