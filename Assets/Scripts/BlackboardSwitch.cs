a79290995227125f01034dd1ff21d99a15c9cfb3
wendy <wendy20501@gmail.com>
2015-10-01 20:06:50 -0700

    [merge] merge by myself....so sad

---
+++ Assets/Scripts/BlackboardSwitch.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlackboardSwitch : MonoBehaviour {
+	public Transform Player;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Player.localPosition.x > transform.localPosition.x - 20) {  
+			transform.localPosition = new Vector3(transform.localPosition.x + 120, transform.localPosition.y,transform.localPosition.z);  
+		} 
+	}
+}
---
+++ Assets/Scripts/Parallaxing.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallaxing : MonoBehaviour {
+	public Transform[] Backgrounds;
+	private float[] parallaxScales;
+	public float smoothing = 1f;
+	private Transform cam;
+	private Vector3 previousCamPos;
+
+	void Awake() {
+		cam = Camera.main.transform;
+	}
+
+	void Start () {
+		previousCamPos = cam.position;
+		parallaxScales = new float[Backgrounds.Length];
+		for (int i = 0; i < Backgrounds.Length; i++) {
+			parallaxScales[i] = Backgrounds[i].position.z * -1;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < Backgrounds.Length; i++) {
+			float parallax = (previousCamPos.x - cam.position.x) * parallaxScales[i];
+			float backgroundTargetPosX = Backgrounds[i].position.x + parallax;
+			Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, Backgrounds[i].position.y, Backgrounds[i].position.z);
+			Backgrounds[i].position = Vector3.Lerp(Backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+		}
+		previousCamPos = cam.position;
+	}
+}
